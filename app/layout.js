71b3831c5c35cde192b284f15ea609fe960aa8a4
sandipthemes <sandip.themes@gmail.com>
2023-08-01 10:51:02 +0530

    Updated Metadata in RootLayout

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import 'styles/theme.scss';
 
 export const metadata = {
     title: 'Dash UI - Next.Js Admin Dashboard Template',
-    description: 'Dash UI - Next JS admin template is free and available on GitHub. Create your stunning web apps with our Free Next js template. An open-source admin dashboard built using the new router, server components, and everything new in Next.js 13.',
+    description: 'Dash UI - Next JS admin dashboard template is free and available on GitHub. Create your stunning web apps with our Free Next js template. An open-source admin dashboard built using the new router, server components, and everything new in Next.js 13.',
     keywords: 'Dash UI, Next.js 13, Admin dashboard, admin template, web apps, bootstrap 5, admin theme'
 }
 
